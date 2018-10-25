259f16d04d5a728b566497dfd31ede535a800aca
Erik Wright <erik.wright@shopify.com>
2018-10-25 12:33:34 -0400

    Revert "LMFTFY."
    This reverts commit 000bcd9c470d798c8d5558457a06146135caec26.

--- slots.py
+++ slots.py
@@ -14,11 +14,19 @@ class SlotMachine:
         BELL = 'Bell'
 
     def __init__(self):
+        cfg_path = os.environ.get('SLOTS_CFG_PATH', '.slots.cfg')
+        self.__config = json.load(open(cfg_path)) if cfg_path and os.path.exists(cfg_path) else {}
         self.__deposited = 0
         self.__prize_reserves = 0
-        self.__reels = [self.Symbol.BELL for _ in range(3)]
+        self.__reels = [self.Symbol.BELL for _ in range(self.__config.get('reels', 3))]
+        self.__pay_table = self.__config.get('pay_table', self.__DEFAULT_PAY_TABLE)
 
     def insert_money(self, amount):
+        if self.__deposited:
+            raise Exception('Money already deposited.')
+        minimum_play = self.__config.get('minimum_play', 25)
+        if amount < minimum_play:
+            raise Exception(f'The amount {amount} is below the minimum play {minimum_play}.')
         self.__deposited = amount
 
     def return_money(self):
@@ -26,6 +34,8 @@ class SlotMachine:
         return to_return
 
     def adjust_reserves(self, amount):
+        if self.__prize_reserves + amount < 0:
+            raise Exception('Cannot withdraw more than reserve.')
         self.__prize_reserves += amount
 
     def reserves(self):
@@ -35,10 +45,32 @@ class SlotMachine:
         return list(self.__reels)
 
     def play(self):
+        if not self.__deposited:
+            raise Exception('Insert money prior to playing.')
+        if self.__deposited * max(self.__pay_table.values()) > self.__prize_reserves:
+            raise Exception('Insufficient reserves for maximum possible payout.')
+
         for i in range(len(self.__reels)):
             self.__reels[i] = random.choice(list(self.Symbol))
-        winnings = 0
+        pay_table_key = ' '.join(reel.value for reel in self.__reels)
+        if pay_table_key not in self.__pay_table:
+            counter = collections.Counter(self.__reels)
+            highest_count = counter.most_common(1)[0][1]
+            for i in reversed(range(highest_count)):
+                pay_table_key = ' '.join('*' * (i + 1))
+                if pay_table_key in self.__pay_table:
+                    break
+        winnings = self.__pay_table.get(pay_table_key, 0) * self.__deposited
+
         self.__prize_reserves += self.__deposited - winnings
         self.__deposited = 0
 
         return winnings
+
+    __DEFAULT_PAY_TABLE = {
+        '* *': 1,
+        '* * *': 2,
+        'Diamonds Spades Hearts': 4,
+        'Hearts Hearts Hearts': 8,
+        'Bell Bell Bell': 16
+    }
